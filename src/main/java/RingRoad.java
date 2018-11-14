8b308405b635bc7210fab643ccf27b7b643c3a64
Vineela Vineela <vineela.vini421.vv@gmail.com>
2018-11-14 11:18:12 +0530

    Create RingRoad class which has list of toll gates

---
+++ src/main/java/RingRoad.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RingRoad {
+    private List<TollGate> tollGates = new ArrayList<>();
+
+    public RingRoad() {
+        addTollGates();
+    }
+
+    private void addTollGates() {
+        tollGates.add(1,new TollGate(10,15));
+        tollGates.add(2,new TollGate(25,45));
+        tollGates.add(3,new TollGate(20,35));
+        tollGates.add(4,new TollGate(15,30));
+    }
+}
