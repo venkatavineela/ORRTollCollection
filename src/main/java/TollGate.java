e8755818cec1baa7dbcfacc191f65bb5d057c13b
Vineela Vineela <vineela.vini421.vv@gmail.com>
2018-11-14 11:03:08 +0530

    Create a TollGate class

---
+++ src/main/java/TollGate.java
@@ -0,0 +1,10 @@
+public class TollGate {
+    private int twoWheelerFee;
+    private int fourWheelerFee;
+
+    public TollGate(int twoWheelerFee, int fourWheelerFee) {
+        this.twoWheelerFee = twoWheelerFee;
+        this.fourWheelerFee = fourWheelerFee;
+    }
+
+}
