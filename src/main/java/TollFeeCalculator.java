71358373cf38e1505aff8b4f0ca3f284528b909d
Vineela Vineela <vineela.vini421.vv@gmail.com>
2018-11-19 17:27:14 +0530

    Modify getCalculatedFee method by replacing two loops with one loop

--- src/main/java/TollFeeCalculator.java
+++ src/main/java/TollFeeCalculator.java
@@ -3,16 +3,14 @@ import java.util.List;
 public class TollFeeCalculator {
     int getCalculatedFee(List<TollGate> gatesList,int typeOfVehicle) {
         int fee = 0;
-        if(typeOfVehicle == 2) {
+
             for (TollGate aGatesList : gatesList) {
-                fee += aGatesList.getTwoWheelerFee();
+                if (typeOfVehicle == 2) {
+                    fee += aGatesList.getTwoWheelerFee();
+                } else if (typeOfVehicle == 4) {
+                    fee += aGatesList.getFourWheelerFee();
+                }
             }
-        }
-        else if(typeOfVehicle == 4) {
-            for (TollGate aGatesList : gatesList) {
-                fee += aGatesList.getFourWheelerFee();
-            }
-        }
         return fee;
     }
 }
